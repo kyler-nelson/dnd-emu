5d8224b2db865d75a3e933bb5256aa8ecb1811b4
nukiebaba <relyk92@gmail.com>
2020-03-08 15:38:56 -0700

    and serde and some basic printing of race info

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,14 @@
+extern crate serde;
 extern crate yaml_rust;
 
 use std::fmt::Debug;
 use std::fs::File;
 use std::io::Read;
+
+use serde::{Deserialize, Serialize};
 use yaml_rust::YamlLoader;
 
-#[derive(Debug)]
+#[derive(Serialize, Deserialize, Debug)]
 enum Race {
     Dwarf,
     HillDwarf,
@@ -19,11 +22,11 @@ enum Race {
     HalfOrc,
     Tiefling,
 }
-
+#[derive(Serialize, Deserialize, Debug)]
 struct Subrace {
     parent: Race,
 }
-
+#[derive(Serialize, Deserialize, Debug)]
 enum Alignment {
     LawfulGood,
     NeutralGood,
@@ -37,13 +40,14 @@ enum Alignment {
     Unaligned,
 }
 
+#[derive(Serialize, Deserialize, Debug)]
 enum Size {
     Small,
     Medium,
     Large,
     Huge,
 }
-
+#[derive(Serialize, Deserialize, Debug)]
 enum Language {
     Common,
     Dwarvish,
@@ -62,9 +66,9 @@ enum Language {
     Sylvan,
     Undercommmon,
 }
-
+#[derive(Serialize, Deserialize, Debug)]
 enum Background {}
-
+#[derive(Serialize, Deserialize, Debug)]
 enum Class {
     Barbarian,
     Bard,
@@ -79,7 +83,7 @@ enum Class {
     Warlock,
     Wizard,
 }
-
+#[derive(Serialize, Deserialize, Debug)]
 struct Character {
     race: Race,
     name: &'static str,
@@ -92,19 +96,42 @@ struct Character {
     level: u32,
 }
 
+#[derive(Serialize, Deserialize, Debug)]
+struct RaceInfo {
+    race: Race,
+    age: u32,
+    alignment: Alignment,
+    size: Size,
+    speed: i64,
+    languages: Vec<Language>,
+    experience_points: u32,
+    level: u32,
+}
+
 fn main() {
-    let races = load_races_from_file("./data/races.yaml");
+    let races = load_races_from_file("data/races.yaml");
     println!("{:?}", races);
 }
 
-fn load_races_from_file(file_path: &'static str) -> Vec<Race> {
+fn load_races_from_file(file_path: &'static str) -> Vec<RaceInfo> {
     let mut file = File::open(file_path).unwrap();
     let mut file_contents = String::new();
     file.read_to_string(&mut file_contents).unwrap();
 
     YamlLoader::load_from_str(&file_contents).unwrap();
 
-    let races: Vec<Race> = Vec::new();
+    let mut races: Vec<RaceInfo> = Vec::new();
+
+    races.push(RaceInfo {
+        race: Race::Dwarf,
+        age: 80,
+        alignment: Alignment::ChaoticNeutral,
+        size: Size::Medium,
+        speed: 25,
+        languages: vec![Language::Dwarvish],
+        experience_points: 0,
+        level: 1,
+    });
 
     return races;
 }
